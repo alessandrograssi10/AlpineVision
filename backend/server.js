111cf41807353ab8dcb340b0a83f06224bec0283
ByteMasterMatteo <matteo.giuliani02@gmail.com>
2024-04-16 14:25:09 +0200

    Ho aggiunto cors

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const { connectToDatabase } = require('./config/database');
 const userRoutes = require('./routes/userRoutes');
-const productRoutes=require('./routes/productRoutes');
-const postsRoutes=require('./routes/postRoutes');
+const productRoutes = require('./routes/productRoutes');
+const postsRoutes = require('./routes/postRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());  // Attiva CORS per tutte le routes
 app.use(express.json());
 
 connectToDatabase().then(() => {
     app.use('/api/users', userRoutes);
     app.use('/api/products', productRoutes);
-    app.use('/api/posts',postsRoutes);
-
+    app.use('/api/posts', postsRoutes);
 
     app.listen(PORT, () => {
         console.log(`Server in ascolto sulla porta ${PORT}`);
