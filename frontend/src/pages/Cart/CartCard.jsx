0ca5eb2bef17ed73c953660ebc5f637582126f02
Less6990 <alessio.cece02@gmail.com>
2024-05-03 14:27:14 +0200

    modified cart

--- frontend/src/pages/Cart/CartCard.jsx
+++ frontend/src/pages/Cart/CartCard.jsx
@@ -1,12 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col, Button } from 'react-bootstrap';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './CartCard.css'
-import trashBin from '../../assets/Images/trashBin.png'
+import trashBin from '../../assets/Images/trashBin.png';
 
-function CartCard({ price, quantity, updateTotalPrice }) {
+function CartCard({ price, quantity, updateTotalPrice, prodID}) {
 
     const [qnt, setQnt] = useState(quantity);
+    const [prodName, setProdName] = useState("");
 
     const clickHandle = (e) => {
 
@@ -24,19 +25,43 @@ function CartCard({ price, quantity, updateTotalPrice }) {
         }
     }
 
+    const removeHandleClick = () => {
 
+    }
+
+    useEffect(() => {
+
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const requestOptions = {
+            method: "GET",
+            headers: myHeaders,
+            redirect: "follow"
+        };
+
+        fetch("http://localhost:3000/api/products", requestOptions)
+            .then((response) => response.json())
+            .then((result) => {
+                const product = result.find(product => product._id === prodID);
+                setProdName(product.nome);
+            })
+            .catch((error) => console.error(error));
+
+
+    })
 
 
     return (
         <>
             <Container id="cartMainContainer" className="">
                 <Row className="mt-5">
-                    <h1 className="display-5">Nome prodotto</h1>
+                    <h1 className="display-5">{prodName}</h1>
                 </Row>
                 <Row className="h-100">
 
                     {/* Colonna immagine */}
-                    <Col xs="2" lg="3" xl="3" className="p-1">
+                    <Col xs="2" lg="2" xl="2" className="p-1">
                         <Container id="variantImgContainer" className="bg-danger h-100 d-flex justify-content-center align-items-center">
                             <img id="variantImg" src="" alt="prodotto" />
                         </Container>
@@ -56,11 +81,21 @@ function CartCard({ price, quantity, updateTotalPrice }) {
                     <Col xs="4" lg="4" xl="3" className="d-flex justify-content-center align-items-center pe-3">
                         <h5 className="">{(price * qnt).toFixed(2)}€</h5>
                     </Col>
+                </Row>
 
-                    
-
+                <Row className="mt-2">
+                    <Col xs="6"className="fs-3">
+                        Rimuovi
+                        <Button id="removeButton" className="ms-3">
+                            <div id="trashBinImgDiv">
+                                <img src={trashBin} alt="" />
+                            </div>
+                        </Button>
+                    </Col>
                 </Row>
             </Container>
+            <br />
+            <br />
             <br />
             <br />
             <br />
